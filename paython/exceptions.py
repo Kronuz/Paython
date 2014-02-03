b547e39295e12320ae254e9daf87278c1d0a3c47
German M. Bravo <german.mb@deipi.com>
2014-02-03 10:34:27 -0600

    Simplified Exception types

--- paython/exceptions.py
+++ paython/exceptions.py
@@ -1,43 +1,23 @@
 class DataValidationError(Exception):
     """ Errors when data is corrupt, malformed or just plain wrong """
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
+    pass
 
 
 class MissingDataError(Exception):
     """ Errors when data is missing in developer API call """
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
+    pass
 
 
 class GatewayError(Exception):
     """ Errors returned from API Gateway """
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
+    pass
 
 
 class RequestError(Exception):
     """ Errors during the API Request """
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
+    pass
 
 
 class MissingTranslationError(Exception):
     """ Errors with trying to find a translation"""
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
+    pass
